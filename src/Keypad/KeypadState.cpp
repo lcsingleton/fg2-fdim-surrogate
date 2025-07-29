17ff1624eb2f137c7f7cd87fce6fbb6a37026c02
Lyle Singleton <lyle@agrinous.com>
2025-07-30 00:11:18 +1000

    pseudocode to evaluate pin state

--- src/Keypad/KeypadState.cpp
+++ src/Keypad/KeypadState.cpp
@@ -2,206 +2,141 @@
 
 #include "Buttons.h"
 
+#include <libopencm3/stm32/gpio.h>
+
 using namespace Keypad::KeypadState;
 
+using namespace Keypad::Buttons;
 
-struct KeypadState
+
+enum ButtonStatus
 {
-	//--------------------//
-	// HIWORD             //
-	//--------------------//
-	Button<1> back;
-	// Button<> cd
-	Button<15> eject;
-	Button<22> leftSeek;
-	Button<23> rightSeek;
-	// Button<> media;
-	// Button<> menu;
-	// Button<> ok;
-	// Button<> power;
-	// Button<> scan;
-	Button<31> fmAm;
-
-	//--------------------//
-	// LOWORD             //
-	//--------------------//
-	Button<22> rotaryClockwise;
-	Button<23> rotaryCounterClockwise;
-
-	// Byte 0
-	Button<31> ac;
-	Button<30> airSource;
-	Button<29> rearDemist;
-
-	// Byte 1
-	Button<27> vent;
-	Button<25> hvacAuto;
-	Button<24> hvacOff;
-	Button<23> fanIncrease;
-	Button<22> fanReduce;
-	Button<21> frontDemist;
-
-	// Byte 2
-	Button<15> tempIncrease;
-	Button<14> tempReduce;
-	Button<8> hazard;
-
-	// Byte 3
-	Button<6> lock;
-	Button<5> interiorLight;
-	Button<4> dynamicStabilityControl;
-	Button<2> unlock;
-
-	// Byte 4
-	DataPoint<unsigned char, 0, 3> cabinTemp;
+	BS_UNKNOWN = 0,
+	BS_RELEASED = 1,
+	BS_PRESSED = 2,
 };
 
-//------------------------------------------------------------------------------------------------//
-// Read the status of all the input pins and sensors, then update the stored status of the system //
-//------------------------------------------------------------------------------------------------//
-void UpdateKeypadState()
-	//	{
-	//for( int i = 0; i < FdmButtonType::FBT_COUNT; i++ )
-	//{
-	//	auto pinA = allPins[ i ];
-	//	pinMode( pinA, OUTPUT );
-	//	digitalWrite( pinA, 0 );
-	//	for( int j = 0; j < allPinCount; j++ )
-	//	{
-	//		auto pinB = allPins[ j ];
-	//		if( pinA == pinB )
-	//		{
-	//			continue;
-	//		}
-	//		// if the output pin is not the same pin being read and there is a connection
-	//		if( !digitalRead( pinB ) )
-	//		{
-
-	//			currentState[ j ][ i ] = PinStatus::ON;
-	//		} else
-	//		{
-	//			currentState[ j ][ i ] = PinStatus::OFF;
-	//		}
-	//	}
-	//	// set pin back to input before moving on to the next pin
-	//	pinMode( pinA, INPUT_PULLUP );
-	//}
-
-	//auto hasChanges = false;
-
-	//for( int i = 0; i < allPinCount && !hasChanges; i++ )
-	//{
-	//	for( int j = 0; j < allPinCount; j++ )
-	//	{
-	//		if( j == i )
-	//		{
-	//			continue;
-	//		}
-	//		auto lastValue = lastState[ j ][ i ];
-	//		auto currentValue = currentState[ j ][ i ];
-
-	//		if( lastValue != currentValue )
-	//		{
-	//			hasChanges = true;
-	//			break;
-	//		}
-	//	}
-	//}
-
-	//if( hasChanges )
-	//{
-	//	Serial.println();
+ButtonStatus buttonStates[FdmButtonType::FBT_COUNT] = { 
+	ButtonStatus::BS_UNKNOWN, // FBT_HVAC_OFF
+	ButtonStatus::BS_UNKNOWN, // FBT_RECIRC
+	ButtonStatus::BS_UNKNOWN, // FBT_AC
+	ButtonStatus::BS_UNKNOWN, // FBT_AUTO
+	ButtonStatus::BS_UNKNOWN, // FBT_HAZARD
+	ButtonStatus::BS_UNKNOWN, // FBT_FRONT_DEMIST
+	ButtonStatus::BS_UNKNOWN, // FBT_VENTS
+	ButtonStatus::BS_UNKNOWN, // FBT_REAR_DEMIST
+	ButtonStatus::BS_UNKNOWN, // FBT_PASS_TEMP_MINUS
+	ButtonStatus::BS_UNKNOWN, // FBT_PASS_TEMP_PLUS
+	ButtonStatus::BS_UNKNOWN, // FBT_FAN_MINUS
+	ButtonStatus::BS_UNKNOWN, // FBT_FAN_PLUS
+	ButtonStatus::BS_UNKNOWN, // FBT_DRIVER_TEMP_MINUS
+	ButtonStatus::BS_UNKNOWN, // FBT_DRIVER_TEMP_PLUS
+	ButtonStatus::BS_UNKNOWN, // FBT_EJECT
+	ButtonStatus::BS_UNKNOWN, // FBT_SOURCE_CD
+	ButtonStatus::BS_UNKNOWN, // FBT_SCN_AS
+	ButtonStatus::BS_UNKNOWN, // FBT_SOURCE_MEDIA
+	ButtonStatus::BS_UNKNOWN, // FBT_SOURCE_FM_AM
+	ButtonStatus::BS_UNKNOWN, // FBT_BACK
+	ButtonStatus::BS_UNKNOWN, // FBT_OK
+	ButtonStatus::BS_UNKNOWN, // FBT_MENU
+	ButtonStatus::BS_UNKNOWN, // FBT_AUDIO_POWER
+	ButtonStatus::BS_UNKNOWN, // FBT_SEEK_LEFT
+	ButtonStatus::BS_UNKNOWN, // FBT_SEEK_RIGHT
+	ButtonStatus::BS_UNKNOWN, // FBT_LOCK
+	ButtonStatus::BS_UNKNOWN, // FBT_UNLOCK
+	ButtonStatus::BS_UNKNOWN, // FBT_DSC
+	ButtonStatus::BS_UNKNOWN  // FBT_INTERIOR_LIGHT
+};
 
 
-		//   for (int i = 0; i < allPinCount; i++)
-		//   {
-		//    Serial.print(i% 10);
-		//     for (int j = 0; j < allPinCount; j++)
-		//     {
-		//    Serial.print(j% 10);
-		//       if (j == i)
-		//       {
-		//      Serial.print(" ");
-		//         continue;
-		//       }
-		//       auto lastValue = lastState[j][i];
-		//       auto currentValue = currentState[j][i];
-
-		//    if (lastValue == currentValue)
-		//    {
-		//     Serial.print(" ");
-		//     continue;
-		//    }
-
-		//  if (currentValue == PinStatus::ON ){
-		//      Serial.print("#");
-		//    }
-		//    else
-		//    {
-		//      Serial.print("-");
-
-		//    }
-		//     }
-		//  Serial.println();
-		//   }
-
-//		memcpy( lastButtonState, currentButtonState, sizeof( lastState ) );
-	//}
-
-	// for( unsigned groundPinIndex = 0; groundPinIndex < allPinCount && !hasChanges; groundPinIndex++ )
-	// {
-	//  auto groundPin = allPins[groundPinIndex];
-
-	//  for ( auto signalPinIndex = 0; signalPinIndex < allPinCount; signalPinIndex++ )
-	//  {
-	//    auto lastValue = lastState[signalPinIndex][groundPinIndex];
-	//    auto currentValue = currentState[signalPinIndex][groundPinIndex];
-	//  }
-	// }
-
-	// Serial.print("   ");
-	// for ( auto signalPinIndex = 0; signalPinIndex < allPinCount; signalPinIndex++ )
-	// {
-	//  auto connectorPin = signalPinIndex + 5;
-	//  if(signalPinIndex < 10) {
-	//    Serial.print(" ");
-	//  }
-	//  Serial.print(connectorPin);
-	//  Serial.print(" ");
-
-	// }
-	// Serial.print("\n");
-
-	// for( unsigned groundPinIndex = 0; groundPinIndex < allPinCount; groundPinIndex++ )
-	// {
-
-	//  auto connectorPin = groundPinIndex + 5;
-	//  if(groundPinIndex < 10) {
-	//    Serial.print(" ");
-	//  }
-	//  Serial.print(connectorPin);
-	//  Serial.print(" ");
-
-	f/*or( auto signalPinIndex = 0; signalPinIndex < allPinCount; signalPinIndex++ )
+void InitKeypadGpios()
+{
+	// Set up the GPIO pins for the keypad buttons
+	for( unsigned i = 0; i < FdmButtonType::FBT_COUNT; i++ )
 	{
-		auto lastValue = lastState[ signalPinIndex ][ groundPinIndex ];
-		auto currentValue = currentState[ signalPinIndex ][ groundPinIndex ];
-		Serial.print( " " );
-		if( currentValue == PinStatus::OFF )
-		{
-			Serial.print( "+" );
-		} else if( currentValue == PinStatus::ON )
-		{
-			Serial.print( "-" );
-		} else
-		{
-			Serial.print( " " );
-		}
-		Serial.print( " " );
-	}
-	Serial.print( "\n" );*/
-	// }
+		auto fdmButton = buttons[ i ];
+		gpio_mode_setup(
+			fdmButton.groundPin, // Port
+			GPIO_MODE_OUTPUT, // Set as output mode
+			GPIO_PUPD_NONE, // Disable the pull up or down
+			fdmButton.groundPin // Pin
+		);
 
-	// }
+		// Set the ground pin to HIGH
+		gpio_clear(
+			GPIOB
+			// Port
+			fdmButton.groundPin // Port
+		);
+	}
+}
+
+void InitKeypadState()
+{
+	InitKeypadGpios();
+
+	InitRotaryEncoder();
+	InitTempSensor();
+}
+
+void UpdateKeypadState()
+{
+	// Clear all of the ground pins and signal pins
+	for ( unsigned i = 0; i < FdmButtonType::FBT_COUNT;
+		i++ )
+	{
+		auto fdmButton = buttons[ i ];
+
+		gpio_clear(
+			GPIOB, // Port B
+			fdmButton.groundPin // Ground Pin
+		);
+		gpio_set(
+			GPIOB, // Port B
+			fdmButton.signalPin // Signal Pin
+		);
+	}
+
+
+	// Perform a matrix scan of the buttons
+	for ( unsigned i = 0; i < FdmButtonType::FBT_COUNT; i++ )
+	{
+		auto fdmButton = buttons[ i ];
+
+		gpio_mode_setup(
+			fdmButton.groundPin, // Port
+			GPIO_MODE_INPUT,
+			GPIO_PUPD_PULLUP, // Enable the pull up resistor
+			fdmButton.groundPin // Pin
+		);
+		gpio_set(
+			GPIOB, // Port B
+			fdmButton.signalPin // Signal Pin
+		);
+
+		// Read the status of the signal pin
+		auto pinStatus = gpio_get(
+			GPIOB, // Port B
+			fdmButton.signalPin // Signal Pin
+		);
+
+		// Set the signal pin back to HIGH
+		gpio_clear(
+			GPIOB, // Port B
+			fdmButton.signalPin // Signal Pin
+		);
+
+		//
+		if ( pinStatus )
+		{
+			buttonStates[ i ] = BS_PRESSED;
+		}
+		else
+		{
+			buttonStates[ i ] = BS_RELEASED;
+		}
+	}
+	
+
 }
 
--- src/Keypad/KeypadState.h
+++ src/Keypad/KeypadState.h
@@ -6,9 +6,52 @@ namespace Keypad
 namespace KeypadState
 {
 
-void UpdateKeypadState();
+// enum RotaryDialMovement
+// {
+// 	ROTARY_NONE,
+// 	ROTARY_CLOCKWISE,
+// 	ROTARY_COUNTER_CLOCKWISE
+// };
 
-}
+// struct State
+// {
+
+// 	bool back = false;
+// 	bool cd = false;
+// 	bool eject = false;
+// 	bool leftSeek = false;
+// 	bool rightSeek = false;
+// 	bool media = false;
+// 	bool menu = false;
+// 	bool ok = false;
+// 	bool power = false;
+// 	bool scan = false;
+// 	bool fmAm = false;
+// 	RotaryDialMovement rotaryDialMovement = ROTARY_NONE;
+// 	bool ac = false;
+// 	bool airSource = false;
+// 	bool rearDemist = false;
+// 	bool vent = false;
+// 	bool hvacAuto = false;
+// 	bool hvacOff = false;
+// 	bool fanIncrease = false;
+// 	bool fanReduce = false;
+// 	bool frontDemist = false;
+// 	bool tempIncrease = false;
+// 	bool tempReduce = false;
+// 	bool hazard = false;
+// 	bool lock = false;
+// 	bool interiorLight = false;
+// 	bool dynamicStabilityControl = false;
+// 	bool unlock = false;
+// 	unsigned char cabinTemp = 0;
+// };
+
+// State &GetState();
+
+void h();
+
+} // namespace KeypadState
 
 } // namespace Keypad
 
